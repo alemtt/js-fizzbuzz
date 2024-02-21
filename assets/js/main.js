f8cc720ae93546f930f8706b92c82b2e89efaa59
alemtt <ale@mattera.eu>
2024-02-21 17:20:15 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,21 @@ const buzz = 'buzz'
 
 for(let i = 1; i < 101; i++){
     console.log(i)
-    const markup = `<li>${i}</li>`
-
+    const markup = `<li>${i}</li>`; 
+    const elementFizz = `<li class="box_fizz">${fizz}</li>`;
+    const buzzelement = `<li class="box_buzz">${buzz}</li>`
+    const elementfizzbuzz = `<li class="box_fizzbuzz">${fizz}${buzz}</li>`
+    console.log(markup);
     if(i % 3 == 0){
-        console.log(markup);
-        listElement.innerHTML += markup
+        listElement.innerHTML += elementFizz
+    
     } else if (i % 5 == 0){
-
-    }else (i % 5 ==0 && i % 3 == 0){
-
+        listElement.innerHTML += buzzelement
+    } else {
+        listElement.innerHTML += markup
     }
-
-
+    if (i % 5 ==0 && i % 3 == 0){
+        listElement.innerHTML += elementfizzbuzz
+    }
+    
 }
